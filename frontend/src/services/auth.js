98d8bb7237446fe9805685309167702772f9add8
Esteban <esteb.gtz.mtz@hotmail.com>
2020-03-04 15:57:46 -0600

    frontend changes
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000';
+const baseURL = 'https://finalprojectironhackmodule3.herokuapp.com';
 
 const MY_SERVICE = axios.create({
     baseURL,
